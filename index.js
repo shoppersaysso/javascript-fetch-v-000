85f9d1480103d4bfba70b629c620530d4847f8e9
Sara Hopper <sara.hopper@gmail.com>
2017-09-13 17:31:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-let xhr = new XMLHttpREquest();
-xhr.open('GET','https://api.github.com/repos/jquery/jquery/commits');
-xhr.responseType = 'json';
-
-xhr.onload = function() {
-  console.log(xhr.response);
-};
-
-xhr.onerror = function() {
-  console.log('Booo');
-};
-
-xhr.send();
+fetch('https://api.github.com/repos/jquery/jquery/commits')
+  .then(res => res.json())
+  .then(json => console.log(json));
