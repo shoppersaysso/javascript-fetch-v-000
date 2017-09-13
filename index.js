d154d292c8623526147cc928c2078fa3ba2f5b9f
Sara Hopper <sara.hopper@gmail.com>
2017-09-13 20:15:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const token = '760869047ff1c29982d033c65ce549b346dfeebc'
-fetch('https://api.github.com/shoppersaysso/repos')
+fetch('https://api.github.com/user/repos')
   headers: {
     Authorization: `token ${token}`
   }
