8fd83babd8d5dc631a2c2ff87fee98d22275d249
Sara Hopper <sara.hopper@gmail.com>
2017-09-13 20:14:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-fetch('https://api.github.com/repos/jquery/jquery/commits')
+const token = '760869047ff1c29982d033c65ce549b346dfeebc'
+
+fetch('https://api.github.com/shoppersaysso/repos')
   .then(res => res.json())
   .then(json => console.log(json));
